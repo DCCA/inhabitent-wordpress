aaf8fa6b79a339f17274c2471e9b4ed4d92b17e9
Daniel C C Andrade <dcca12@gmail.com>
2020-02-24 11:39:46 -0800

    add font awesome cdn

--- themes/inhabitent-theme/functions.php
+++ themes/inhabitent-theme/functions.php
@@ -84,6 +84,7 @@ add_filter( 'stylesheet_uri', 'inhabitent_starter_minified_css', 10, 2 );
  */
 function inhabitent_starter_scripts() {
 	wp_enqueue_style( 'inhabitent-starter-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'inhabitent-starter-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css');
 
 	wp_enqueue_script( 'inhabitent-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'inhabitent-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
