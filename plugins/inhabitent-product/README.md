1d394a920031617fdbe1ed811d211beec9453356
Daniel C C Andrade <dcca12@gmail.com>
2020-02-26 10:52:18 -0800

    add journal custom loop to the front page

--- plugins/inhabitent-product/README.md
+++ plugins/inhabitent-product/README.md
@@ -1,3 +1,7 @@
 # Inhabitent Product
 
 Add the product custom post to the Inhabitent Theme
+
+## To Do
+
+[ ] Add custom fonts
--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -9,15 +9,29 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<!-- <img src="<?php echo get_template_directory_uri() . '/assets/logos/inhabitent-logo-full.svg' ?>" alt="" class="inhabitent-logo-front-page"> -->
+		
+			<?php
+				$args = array( 
+					'post_type'      => 'post', 
+					'posts_per_page'  => 3,
+				);
+				$journal_posts = get_posts( $args ); // returns an array of posts
 			
-			<?php  while ( have_posts() ) : the_post(); ?>
+			?>
 
-				<?php  get_template_part( 'template-parts/content', 'page' ); ?>
+			<div class='inhabitent-journal-post-cards-container'>
+				<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+					
+					<div class='inhabitent-journal-post-cards'>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'medium' ); ?>
+						<?php endif; ?>
+						<p><?php echo get_the_date() . ' / ' . get_comments_number() . ' Comments'?></p>
+						<h1><?php the_title() ?></h1>
+					</div>
 
-			<?php  endwhile; // End of the loop. ?>
-
+				<?php endforeach; wp_reset_postdata(); ?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
