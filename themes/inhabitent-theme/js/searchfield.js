b9c4cb17c386f9862a51552f5fe4056b4865fad0
DCCA <dcca12@gmail.com>
2020-02-29 14:24:37 -0800

    add the search bar functionality

--- themes/inhabitent-theme/js/searchfield.js
+++ themes/inhabitent-theme/js/searchfield.js
@@ -2,6 +2,14 @@
  * Hide and expand the input field of the search bar in header
  */
 (function() {
+    const searchBtn = document.querySelector('.search-submit');
     const searchInput = document.getElementById('search-field-header');
+    console.log(searchBtn);
     console.log(searchInput);
+    searchBtn.addEventListener('click', event => {
+      event.preventDefault();
+      console.log('clicked');
+      searchInput.classList.toggle('search-field-hidden');
+      searchInput.classList.toggle('search-field-show');
+    })
   })();
--- themes/inhabitent-theme/searchform.php
+++ themes/inhabitent-theme/searchform.php
@@ -1,13 +1,11 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<fieldset>
-		<button class="search-submit">
-			<span class="icon-search" aria-hidden="true">
-				<i class="fa fa-search"></i>
-			</span>
-			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
-		</button>
+	<fieldset style='display: inline;'>
+		<a href='#' class="search-submit">
+			<i class="icon-search fa fa-search"></i>
+		</a>
 		<label>
-			<input id='search-field-header' type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input id='search-field-header' type="search" class="search-field-hidden" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
+		<input type="submit" id="search-submit" class="screen-reader-text" value="Search">
 	</fieldset>
 </form>
