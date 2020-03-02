dab0f9128f69aa9cfab6c717c8d4082f1ca42162
DCCA <dcca12@gmail.com>
2020-03-02 10:08:30 -0800

    add bold to the days of the week text in the business widget

--- plugins/inhabitent-widget/views/widget.php
+++ plugins/inhabitent-widget/views/widget.php
@@ -1,12 +1,12 @@
 <!-- This file is used to markup the public-facing widget. -->
 <div class="text-widget">
     <?php if (strlen(trim($weekdays)) > 0) : ?>
-        <p>Monday-Fridays: <?php echo $weekdays; ?></p>
+        <p><span class='days-of-the-week'>Monday-Fridays: </span> <?php echo $weekdays; ?></p>
     <?php endif; ?>
     <?php if (strlen(trim($saturdays)) > 0) : ?>
-        <p>Saturday: <?php echo $saturdays; ?></p>
+        <p><span class='days-of-the-week'>Saturday: </span><?php echo $saturdays; ?></p>
     <?php endif; ?>
     <?php if (strlen(trim($sundays)) > 0) : ?>
-        <p>Sundays: <?php echo $sundays; ?></p>
+        <p><span class='days-of-the-week'>Sundays: </span><?php echo $sundays; ?></p>
     <?php endif; ?>
 </div>
